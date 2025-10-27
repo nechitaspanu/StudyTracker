17c63cac638b04a16e2d7abfaec67db1201766c9
nechitaspanu <nechitaspanu@gmail.com>
2025-10-27 20:33:01 +0200

    tema 1 test 3

--- main.cpp
+++ main.cpp
@@ -225,49 +225,15 @@ class CalendarEvent {
         }
     };
 
-
-
-
-
 int main() {
 
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-
-    /////////////////////////////////////////////////////////////////////////
-
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
     ios::sync_with_stdio(false);
     StudyTracker st;
 
     //cursuri
-    st.addCourse(Course("POO", 12, 7));
+    Course poo{"POO", 12, 7};
+    poo.completeUnits(2);
+    st.addCourse(poo);
     st.addCourse(Course("Algoritmi", 10, 4));
     st.addCourse(Course("Baze de date", 8, 2));
 
@@ -285,53 +251,5 @@ int main() {
     cout << "\nDeadline in urmatoarele 14 zile:\n";
     for (const auto& a : soon) cout << "* " << a << "\n";
 
-
-
-   // sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    // window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //std::cout << "Fereastra a fost creată\n";
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-   // window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
-
-    //while(window.isOpen()) {
-        //bool shouldExit = false;
-
-       // while(const std::optional event = window.pollEvent()) {
-            //if (event->is<sf::Event::Closed>()) {
-                //window.close();
-                //std::cout << "Fereastra a fost închisă\n";
-           // }
-           // else if (event->is<sf::Event::Resized>()) {
-                //std::cout << "New width: " << window.getSize().x << '\n'
-                         // << "New height: " << window.getSize().y << '\n';
-           // }
-           // else if (event->is<sf::Event::KeyPressed>()) {
-              //  const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
-             //   std::cout << "Received key " << (keyPressed->scancode == sf::Keyboard::Scancode::X ? "X" : "(other)") << "\n";
-               // if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
-                  //  shouldExit = true;
-            //    }
-          //  }
-    //    }
-      //  if(shouldExit) {
-          //  window.close();
-           // std::cout << "Fereastra a fost închisă (shouldExit == true)\n";
-            //break;
-       // }
-       // using namespace std::chrono_literals;
-      //  std::this_thread::sleep_for(300ms);
-
-       // window.clear();
-     //   window.display();
-   // }
-
-    //std::cout << "Programul a terminat execuția\n";
     return 0;
 }
